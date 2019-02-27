3593464c99598154be80495a0b6805e8e37cca16
Akash Bhiwgade <32064265+Akash-Bhiwgade@users.noreply.github.com>
2019-02-27 16:37:40 +0530

    Add files via upload

---
+++ face_emotion_recognition.py
@@ -0,0 +1,90 @@
+import numpy as np
+
+filname = 'fer2013.csv'
+label_map = ['Anger', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
+
+X = []
+Y = []
+
+first = True
+for line in open(filname):
+    if first:
+        first = False
+    else:
+        row = line.split(',')
+        Y.append(int(row[0]))
+        X.append([int(p) for p in row[1].split()])
+
+X, Y = np.array(X) / 255.0, np.array(Y)
+
+X, Y = getData(filname)
+num_class = len(set(Y))
+
+num_class = set(Y)
+count_class = {}
+for i in range(len(num_class)):
+    balance[i] = sum([1 for y in Y if y == i])
+
+N, D = X.shape
+X = X.reshape(N, 48, 48, 1)
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.1, random_state=0)
+y_train = (np.arange(num_class) == y_train[:, None]).astype(np.float32)
+y_test = (np.arange(num_class) == y_test[:, None]).astype(np.float32)
+
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+
+batch_size = 128
+epochs = 124
+
+# Initialising the CNN
+model = Sequential()
+
+# 1 - Convolution
+model.add(Conv2D(64,(3,3), padding='same', input_shape=(48, 48,1)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+# 2nd Convolution layer
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+# 3rd Convolution layer
+model.add(Conv2D(512,(3,3), padding='same'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+# 4th Convolution layer
+model.add(Conv2D(512,(3,3), padding='same'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+# Flattening
+model.add(Flatten())
+
+# Fully connected layer 1st layer
+model.add(Dense(256))
+model.add(Activation('relu'))
+model.add(Dropout(0.25))
+
+# Fully connected layer 2nd layer
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dropout(0.25))
+
+model.add(Dense(num_class, activation='sigmoid'))
+
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, verbose=2, validation_split=0.1111)
+
+# Save entire model to a HDF5 file
+model.save('./saved-model/fer-124epoch.h5')
